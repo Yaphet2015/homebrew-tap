3d6cf87274ba4e04b55150565ee77d0a05a1e0cf
Yaphet <yaphetyan@foxmail.com>
2026-08-23 21:29:03 +0800

    ya-skills 0.8.0: sync test block version

--- Formula/ya-skills.rb
+++ Formula/ya-skills.rb
@@ -16,7 +16,7 @@ class YaSkills < Formula
   end
 
   test do
-    assert_match "0.7.0", shell_output("#{bin}/yk --version")
+    assert_match "0.8.0", shell_output("#{bin}/yk --version")
     assert_match "pbench", shell_output("#{bin}/yk list")
   end
 end
